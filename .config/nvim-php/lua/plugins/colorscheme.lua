55f3e3b5d40f5b70f8df2ab84da315e0728df2ae
khauvannam <nkhauvan@gmail.com>
2025-06-02 06:59:39 +0700

    Sync on 2025-06-02 06:59:39

--- .config/nvim-php/lua/plugins/colorscheme.lua
+++ .config/nvim-php/lua/plugins/colorscheme.lua
@@ -1,8 +1,32 @@
 return {
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "catppuccin",
-  --   },
-  -- },
+  {
+    "ellisonleao/gruvbox.nvim",
+  },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_diagnostic_line_highlight = 1
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "gruvbox-material",
+      },
+    },
+  },
 }
